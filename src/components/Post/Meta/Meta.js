4f84340a404bc5dfd3001cb8a3a1641909571483
Mauricio Serna Florez <msflore@bancolombia.com.co>
2020-02-08 13:13:50 -0500

    update: published to publicado

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import moment from 'moment';
-import styles from './Meta.module.scss';
+import React from 'react'
+import moment from 'moment'
+import styles from './Meta.module.scss'
 
 const Meta = ({ date }) => (
   <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Published {moment(date).format('D MMM YYYY')}</p>
+    <p className={styles['meta__date']}>
+      Publicado {moment(date).format('D MMM YYYY')}
+    </p>
   </div>
-);
+)
 
-export default Meta;
+export default Meta
